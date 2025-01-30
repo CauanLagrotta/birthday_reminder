7c7e295ea823d4ee81152e367782220b4e1ffa6a
CauanLagrotta <caun06lagrotta@gmail.com>
2025-01-30 18:29:17 -0300

    arrumando a configuração do typescript

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
-import express from "express";
+import express from 'express'
+import cors from 'cors'
 
 const app = express()
+
 app.listen(3000, () => {
-    console.log("server running on http://localhost:3000")
+    console.log('server running on http://localhost:3000')
 })
